67b0c8bc13682ba1b5e6c0af25d60334cafca1a3
mhalpenny <Matthew@Hosaka.local>
2018-08-11 15:13:20 -0400

    reformatted for new design

--- sketch.js
+++ sketch.js
@@ -1,74 +1,73 @@
+//-------------------------------------------------------------
+//     Created by Matthew Halpenny for Art Matters 2019
+//-------------------------------------------------------------
+
+
+//-------------------------------------------------------------
+//     VARIABLES
+//-------------------------------------------------------------
+
 var animateR, animateA, animateCO = false;
 var fadeR, fadeA, fadeCO;
-var offset;
+var linkOffset = 60;
+var linkBuffer = 60;
 var layerR, layerA, layerCO;
-// var xoff = 0.0;
-// var perlin = 0;
-// var paraR, ppR = 400;
 
+
+//-------------------------------------------------------------
+//     PRELOAD
+//-------------------------------------------------------------
 
 function preload() {
 
-    layerR = loadImage('assets/layerR.png');
-    layerA = loadImage('assets/layerA.png');
-    layerCO = loadImage('assets/layerCO.png');
-    amLogo = loadImage('assets/amlogo.png');
+  layerR = loadImage('assets/layerR.png');
+  layerA = loadImage('assets/layerA.png');
+  layerCO = loadImage('assets/layerCO.png');
+  amLogo = loadImage('assets/amlogo.png');
 }
+
+//-------------------------------------------------------------
+//     SETUP
+//-------------------------------------------------------------
 
 function setup() {
 
-  //---SETUP---
   frameRate(60);
   canvas = createCanvas(windowWidth, windowHeight);
   canvas.style("z-index", "-1");
-  canvas.position(0,0);
+  canvas.position(0, 0);
 
-  //---LINKS---
+  //-------------------------------------------------------------
+  //     LINKS (SETUP)
+  //-------------------------------------------------------------
+
   // optionally migrate to CSS for all links
 
   linkR = createA('#', 'RESOURCES');
-  linkR.style('text-decoration', 'none');
-  linkR.style('color', 'black');
-  linkR.style('letter-spacing', '5');
 
   linkA = createA('#', 'ABOUT');
-  linkA.style('text-decoration', 'none');
-  linkA.style('color', 'black');
-  linkA.style('letter-spacing', '5');
 
   linkCU = createA('#', 'CONTACT US');
-  linkCU.style('text-decoration', 'none');
-  linkCU.style('color', 'black');
-  linkCU.style('letter-spacing', '5');
 
   linkAr = createA('#', 'ARCHIVE');
-  linkAr.style('text-decoration', 'none');
-  linkAr.style('color', 'black');
-  linkAr.style('letter-spacing', '5');
 
   linkGI = createA('#', 'GET INVOLVED');
-  linkGI.style('text-decoration', 'none');
-  linkGI.style('color', 'black');
-  linkGI.style('letter-spacing', '5');
 
   linkE = createA('#', 'EVENTS');
-  linkE.style('text-decoration', 'none');
-  linkE.style('color', 'black');
-  linkE.style('letter-spacing', '5');
 
   linkCO = createA('#', 'CALL-OUTS');
-  linkCO.style('text-decoration', 'none');
-  linkCO.style('color', 'black');
-  linkCO.style('letter-spacing', '5');
+
 
   linkAO = createA('pdf/AM_AntiO.pdf', 'ANTI-OPPRESSION STATEMENT');
-  linkAO.style('letter-spacing', '2');
 
   linkEN = createA('index.html', 'EN');
   linkFR = createA('indexFR.html', 'FR');
 
 
-//---EVENTS---
+  //-------------------------------------------------------------
+  //     EVENTS (SETUP)
+  //-------------------------------------------------------------
+
 
   linkR.mouseOver(overLinkR);
   linkR.mouseOut(offLinkR);
@@ -84,7 +83,83 @@ function setup() {
 
 }
 
-//---HOVER---
+//-------------------------------------------------------------
+//     DRAW
+//-------------------------------------------------------------
+
+
+function draw() {
+
+  noStroke();
+  background(255, 30);
+
+  //positioning
+  linkR.position(20, linkBuffer + linkOffset);
+  linkA.position(20, linkBuffer + linkOffset*2);
+  linkCU.position(20, linkBuffer + linkOffset*3);
+  linkE.position(20, linkBuffer + linkOffset*4);
+  linkGI.position(20, linkBuffer + linkOffset*5);
+  linkAr.position(20, linkBuffer + linkOffset*6);
+  linkCO.position(20, linkBuffer + linkOffset*7);
+  linkAO.position(20, linkBuffer + linkOffset*8);
+  // fill(0);
+  // rect(windowWidth-60, 10, 60, 20);
+  linkEN.position(windowWidth - 60, 15);
+  linkFR.position(windowWidth - 35, 15);
+
+
+
+
+  //-------------------------------------------------------------
+  //     PNG ANIMATIONS (DRAW)
+  //-------------------------------------------------------------
+
+  //---R---
+  tint(255, fadeR);
+  image(layerR, 70, -100);
+
+  var c = color(255, 0, 255, fadeR);
+  fill(c);
+
+  if (animateR == true && fadeR <= 255) {
+    fadeR += 10;
+  }
+  //---A---
+  tint(255, fadeA);
+  image(layerA, 70, -100);
+
+  var c = color(255, 0, 255, fadeA);
+  fill(c);
+
+  if (animateA == true && fadeA <= 255) {
+    fadeA += 10;
+  }
+  //---CO---
+  tint(255, fadeCO);
+  image(layerCO, 70, -100);
+
+  var c = color(255, 0, 255, fadeCO);
+  fill(c);
+
+  if (animateCO == true && fadeCO <= 255) {
+    fadeCO += 10;
+  }
+
+  //-------------------------------------------------------------
+  //     LOGO ANIMATION (DRAW)
+  //-------------------------------------------------------------
+
+  imageMode(CENTER);
+  tint(255, 255);
+  image(amLogo, 60, 50, 95, 95);
+
+}
+
+
+//-------------------------------------------------------------
+//     FUNCTIONS
+//-------------------------------------------------------------
+
 
 function overLinkR() {
   animateR = true;
@@ -96,12 +171,6 @@ function offLinkR() {
   fadeR = 0;
   linkR.style('font-style', 'normal');
 }
-
-// function cascadeR(){
-//   paraR = createP('RESOURCES');
-//   paraR.position(ppR, ppR);
-//   ppR = ppR - 20;
-// }
 
 function overLinkA() {
   animateA = true;
@@ -119,73 +188,6 @@ function overLinkCO() {
 function offLinkCO() {
   animateCO = false;
   fadeCO = 0;
-}
-
-
-//---***DRAW***-------------------------------------
-
-function draw() {
-
-  noStroke();
-  background(255, 30);
-
-  //positioning
-
-  offset = (windowWidth*0.1);
-  // perlinX = noise((xoff*3));
-  // perlinY = noise((xoff*2));
-  // xoff = xoff + 0.01;
-  linkR.position((windowWidth/2)+(offset*1.4), (windowHeight/2)-(20));
-  linkA.position((windowWidth/2), (windowHeight/2)+offset*1.3);
-  linkCU.position((windowWidth/2)-offset, (windowHeight/2)-offset*1.3);
-  linkE.position((windowWidth/2)-offset*2, (windowHeight/2)+offset*1.2);
-  linkGI.position((windowWidth/2)+(offset*1.1), (windowHeight/2)+(offset/2));
-  linkAr.position((windowWidth/2)-offset*2.5, (windowHeight/2)+(offset/2));
-  linkCO.position((windowWidth/2)-offset*3, (windowHeight/2)-(offset/3));
-  linkAO.position(15, 15);
-  // fill(0);
-  // rect(windowWidth-60, 10, 60, 20);
-  linkEN.position(windowWidth-60, 15);
-  linkFR.position(windowWidth-35, 15);
-
-//---LINK ANIMATION---
-
-
-//---ANIMATION---
-//---R---
-  tint(255, fadeR);
-  image(layerR, 70, -100);
-
-  var c = color( 255, 0, 255, fadeR);
-  fill(c);
-
-  if (animateR == true && fadeR <=255){
-    fadeR += 10;
-  }
-  //---A---
-  tint(255, fadeA);
-  image(layerA, 70, -100);
-
-  var c = color( 255, 0, 255, fadeA);
-  fill(c);
-
-  if (animateA == true && fadeA <=255){
-    fadeA += 10;
-  }
-  //---CO---
-  tint(255, fadeCO);
-  image(layerCO, 70, -100);
-
-  var c = color( 255, 0, 255, fadeCO);
-  fill(c);
-
-  if (animateCO == true && fadeCO <=255){
-    fadeCO += 10;
-  }
-
-  tint(255, 255);
-  image(amLogo, (width/2)-60, (height/2)-60);
-
 }
 
 function windowResized() {

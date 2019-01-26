b84fbe05ce1068aae2a830749fff0788fa22727d
mhalpenny <matt.halpenny@gmail.com>
2019-01-26 15:28:21 -0500

    Home page interactivity box
    - Added new cropped images
    - higher res but overall lower file size
    - fixed to camelcase
    - temporarily in box folder

--- EN/js/sketch.js
+++ EN/js/sketch.js
@@ -28,7 +28,7 @@ let fadeR = 0,
   fadeGI = 0,
   fadeEx = 0,
   fadeMain = 255;
-let linkOffset, linkBuffer;
+let linkOffset, linkBuffer, linkMargin;
 let layerR, layerA, layerM, layerE, layerAr, layerC, layerGI, layerEx;
 let iconR, iconA, iconM, iconE, iconAr, iconC, iconGI, iconEx;
 let logoSpin;
@@ -51,14 +51,14 @@ let newWidth;
 function preload() {
 
   //layers
-  layerA = loadImage('assets/RedLayer.png');
-  layerR = loadImage('assets/BlueLayer.png');
-  layerM = loadImage('assets/VioletLayer.png');
-  layerE = loadImage('assets/GrayLayer.png');
-  layerC = loadImage('assets/GreenLayer.png');
-  layerAr = loadImage('assets/OrangeLayer.png');
-  layerGI = loadImage('assets/YellowLayer.png');
-  layerMain = loadImage('assets/LayerAll.png');
+  layerA = loadImage('assets/box/redLayer.png');
+  layerR = loadImage('assets/box/blueLayer.png');
+  layerM = loadImage('assets/box/violetLayer.png');
+  layerE = loadImage('assets/box/greyLayer.png');
+  layerC = loadImage('assets/box/greenLayer.png');
+  layerAr = loadImage('assets/box/orangeLayer.png');
+  layerGI = loadImage('assets/box/yellowLayer.png');
+  layerMain = loadImage('assets/LayerAllBox.png');
   //icons
   iconA = loadImage('assets/rrIcon.png');
   iconR = loadImage('assets/bIcon.png');
@@ -111,30 +111,49 @@ function setup() {
   linkFR.id('lang');
 
 
-  //SETUP only
+  //-------------------------------------------------------------
+  //     NAV FORMATTING (SETUP)
+  //-------------------------------------------------------------
+
 
   //Nav formatting
   if (windowWidth > 650) {
     linkOffset = 55 + (windowHeight) * 0.01
     linkBuffer = 75 - (windowHeight) * 0.005
-    var linkMargin = 35;
+    linkMargin = 35;
   } else {
     linkOffset = 75 - (windowHeight) * 0.05
     linkBuffer = 80 - (windowHeight) * 0.015
-    var linkMargin = 29;
+    linkMargin = 29;
   }
 
   //positioning
   linkA.position(linkMargin, linkBuffer + linkOffset);
   linkR.position(linkMargin, linkBuffer + linkOffset * 2);
   linkGI.position(linkMargin, linkBuffer + linkOffset * 3);
-  linkM.position(linkMargin, linkBuffer + linkOffset * 4);
-  linkC.position(linkMargin, linkBuffer + linkOffset * 5);
-  linkE.position(linkMargin, linkBuffer + linkOffset * 6);
-  // linkAO.position(linkMargin, linkBuffer + linkOffset*7);
-  linkAr.position(linkMargin, linkBuffer + linkOffset * 7);
+  linkE.position(linkMargin, linkBuffer + linkOffset * 4);
+  linkM.position(linkMargin, linkBuffer + linkOffset * 5);
+  linkC.position(linkMargin, linkBuffer + linkOffset * 6);
+  linkEx.position(linkMargin, linkBuffer + linkOffset * 7);
+  linkAr.position(linkMargin, linkBuffer + linkOffset * 8);
 
-  // linkFR.position(windowWidth - 40, 50);
+  var xPos = (60 + (windowWidth / 50));
+  linkFR.position(windowWidth - xPos, 30);
+
+
+  //-------------------------------------------------------------
+  //     LAYERS FORMATTING (SETUP)
+  //-------------------------------------------------------------
+
+  //align with the cnter of the page
+  imageMode(CORNER);
+
+  el = document.getElementById('animContainer');
+  elBound = el.getBoundingClientRect();
+  drawX = elBound.left;
+  drawY = elBound.top;
+  drawW = elBound.right-elBound.left;
+  drawH = elBound.bottom-elBound.top;
 
 
 
@@ -184,70 +203,14 @@ function draw() {
 
   }
 
-  //Nav formatting
-  if (windowWidth > 650) {
-    linkOffset = 55 + (windowHeight) * 0.01
-    linkBuffer = 75 - (windowHeight) * 0.005
-    var linkMargin = 35;
-  } else {
-    linkOffset = 75 - (windowHeight) * 0.05
-    linkBuffer = 80 - (windowHeight) * 0.015
-    var linkMargin = 29;
-  }
-
-  // if (windowWidth > 650) {
-  //   var buttonFade = 50 + (fadeMain/3);
-  //   //Buttons
-  //   fill(252, 19, 100, buttonFade);
-  //   rect(linkMargin - 10, (linkBuffer + linkOffset) - 15, 120, 55, 20);
-  //   fill(17, 66, 81, buttonFade);
-  //   rect(linkMargin - 10, (linkBuffer + linkOffset * 2) - 15, 188, 55, 20);
-  //   fill(239, 196, 88, buttonFade);
-  //   rect(linkMargin - 10, (linkBuffer + linkOffset * 3) - 15, 232, 55, 20);
-  //   fill(252, 19, 100, buttonFade);
-  //   rect(linkMargin - 10, (linkBuffer + linkOffset * 4) - 15, 132, 55, 20);
-  //   if (animateC == false) {
-  //     fill(17, 66, 81, buttonFade);
-  //     rect(linkMargin - 10, (linkBuffer + linkOffset * 5) - 15, 178, 55, 20);
-  //   }
-  //   if (animateM == false) {
-  //     fill(239, 196, 88, buttonFade);
-  //     rect(linkMargin - 10, (linkBuffer + linkOffset * 6) - 15, 100, 55, 20);
-  //   }
-  //   fill(252, 19, 100, buttonFade);
-  //   rect(linkMargin - 10, (linkBuffer + linkOffset * 7) - 15, 150, 55, 20);
-  // }
-
-  //positioning
-  linkA.position(linkMargin, linkBuffer + linkOffset);
-  linkR.position(linkMargin, linkBuffer + linkOffset * 2);
-  linkGI.position(linkMargin, linkBuffer + linkOffset * 3);
-  linkE.position(linkMargin, linkBuffer + linkOffset * 4);
-  linkM.position(linkMargin, linkBuffer + linkOffset * 5);
-  linkC.position(linkMargin, linkBuffer + linkOffset * 6);
-  linkEx.position(linkMargin, linkBuffer + linkOffset * 7);
-  linkAr.position(linkMargin, linkBuffer + linkOffset * 8);
-
-  var xPos = (60 + (windowWidth / 50));
-
-  linkFR.position(windowWidth - xPos, 30);
-
-
 
 
   //-------------------------------------------------------------
-  //     PNG ANIMATIONS (DRAW)
+  //     LAYER & ICON ANIMATIONS (DRAW)
   //-------------------------------------------------------------
 
   //align with the cnter of the page
   imageMode(CORNER);
-
-  el = document.getElementById('animContainer');
-  elBound = el.getBoundingClientRect();
-  drawX = elBound.left;
-  drawY = elBound.top;
-  drawW = elBound.right-elBound.left;
-  drawH = elBound.bottom-elBound.top;
 
   //---MAIN---
   if (mainOff == false) {
@@ -270,11 +233,12 @@ function draw() {
   }
 
 
-
   //---R---
   if (animateR == true) {
     tint(255, fadeR);
+    imageMode(CORNER);
     image(layerR, drawX, drawY, drawW, drawH);
+    imageMode(CENTER);
     image(iconR, linkMargin, (linkBuffer + linkOffset * 2), 50, 50);
     noTint();
     fadeR += fadeIncr;
@@ -283,7 +247,9 @@ function draw() {
   //---A---
   if (animateA == true) {
     tint(255, fadeA);
+    imageMode(CORNER);
     image(layerA, drawX, drawY, drawW, drawH);
+    imageMode(CENTER);
     image(iconA, linkMargin, (linkBuffer + linkOffset), 50, 50);
     noTint();
     fadeA += fadeIncr;
@@ -292,7 +258,9 @@ function draw() {
   //---GI---
   if (animateGI == true) {
     tint(255, fadeGI);
+    imageMode(CORNER);
     image(layerGI, drawX, drawY, drawW, drawH);
+    imageMode(CENTER);
     image(iconGI, linkMargin, (linkBuffer + linkOffset * 3), 50, 50);
     noTint();
     fadeGI += fadeIncr;
@@ -301,7 +269,9 @@ function draw() {
   //---Ar---
   if (animateAr == true) {
     tint(255, fadeAr);
+    imageMode(CORNER);
     image(layerAr, drawX, drawY, drawW, drawH);
+    imageMode(CENTER);
     image(iconAr, linkMargin, (linkBuffer + linkOffset * 8), 50, 50);
     noTint();
     fadeAr += fadeIncr;
@@ -310,14 +280,10 @@ function draw() {
   //---M---
   if (animateM == true) {
     tint(255, fadeM);
+    imageMode(CORNER);
     image(layerM, drawX, drawY, drawW, drawH);
+    imageMode(CENTER);
     image(iconM, linkMargin, (linkBuffer + linkOffset * 5), 50, 50);
-
-    // if (drawW > 650) {
-    //   fill(239, 196, 88, 50);
-    //   rect(linkMargin - 10, (linkBuffer + linkOffset * 6) - 15, 225, 55, 20);
-    // }
-    // linkM.html('COMING SOON');
     noTint();
     fadeM += fadeIncr;
   }
@@ -325,37 +291,32 @@ function draw() {
   //---C---
   if (animateC == true) {
     tint(255, fadeC);
+    imageMode(CORNER);
     image(layerC, drawX, drawY, drawW, drawH);
+    imageMode(CENTER);
     image(iconC, linkMargin, (linkBuffer + linkOffset * 6), 50, 50);
-
-    // if (drawW > 650) {
-    //   fill(17, 66, 81, 50);
-    //   rect(linkMargin - 10, (linkBuffer + linkOffset * 5) - 15, 225, 55, 20);
-    // }
-    // noTint();
-    // linkC.html('COMING SOON');
     fadeC += fadeIncr;
   }
 
   //---E---
   if (animateE == true) {
     tint(255, fadeE);
+    imageMode(CORNER);
     image(layerE, drawX, drawY, drawW, drawH);
+    imageMode(CENTER);
     image(iconE, linkMargin, (linkBuffer + linkOffset * 4), 50, 50);
-
     noTint();
-
     fadeE += fadeIncr;
   }
 
   //---E---
   if (animateEx == true) {
     tint(255, fadeEx);
-    // image(layerE, drawX, drawY, drawW, drawH);
+    imageMode(CORNER);
+    // image(layerEx, drawX, drawY, drawW, drawH);
+    imageMode(CENTER);
     image(iconEx, linkMargin, (linkBuffer + linkOffset * 7), 50, 50);
-
     noTint();
-
     fadeEx += fadeIncr;
   }
 
@@ -364,27 +325,19 @@ function draw() {
   //     LOGO ANIMATION (DRAW)
   //-------------------------------------------------------------
 
-
   angleMode(DEGREES);
   imageMode(CENTER);
 
-  logoSpin = ((mouseY / 2) + (mouseX / 5));
-  push();
   if (windowWidth > 650) {
-    noTint();
-    fill(248, 251, 252, 50);
-    translate(linkMargin * 2.3, 60);
-    rotate(logoSpin);
     tint(255, 255);
-    ellipse(0, 0, 120, 120);
-    image(amLogo, 0, 0, 110, 110);
+    image(amLogo,linkMargin * 2.3, 60, 110, 110);
+    noTint();
   } else {
     tint(255, 255);
-    imageMode(CENTER);
     image(amLogo, 65, 50, 80, 80);
     noTint();
   }
-  pop();
+
 
   //-------------------------------------------------------------
   //     MOBILE ANIMATION (DRAW)
@@ -418,13 +371,11 @@ function draw() {
       animateM = false;
       fadeM = 0;
       animateC = true;
-      linkM.html("MAPS");
       mobileCount++;
     } else if (mobileCount <= 210 && mobileCount > 180) {
       animateC = false;
       fadeC = 0;
       animateE = true;
-      linkC.html("CALENDAR");
       mobileCount++;
     } else if (mobileCount <= 240 && mobileCount > 210) {
       animateE = false;
@@ -586,7 +537,39 @@ function offLang() {
 }
 
 function windowResized() {
-  resizeCanvas(newWidth, (windowHeight));
-  background(248, 251, 252, 50);
+  resizeCanvas(windowWidth, windowHeight);
 
+  var xPos = (60 + (windowWidth / 50));
+  linkFR.position(windowWidth - xPos, 30);
+
+  //Nav formatting
+  if (windowWidth > 650) {
+    linkOffset = 55 + (windowHeight) * 0.01
+    linkBuffer = 75 - (windowHeight) * 0.005
+    var linkMargin = 35;
+  } else {
+    linkOffset = 75 - (windowHeight) * 0.05
+    linkBuffer = 80 - (windowHeight) * 0.015
+    var linkMargin = 29;
+  }
+
+  //positioning
+  linkA.position(linkMargin, linkBuffer + linkOffset);
+  linkR.position(linkMargin, linkBuffer + linkOffset * 2);
+  linkGI.position(linkMargin, linkBuffer + linkOffset * 3);
+  linkE.position(linkMargin, linkBuffer + linkOffset * 4);
+  linkM.position(linkMargin, linkBuffer + linkOffset * 5);
+  linkC.position(linkMargin, linkBuffer + linkOffset * 6);
+  linkEx.position(linkMargin, linkBuffer + linkOffset * 7);
+  linkAr.position(linkMargin, linkBuffer + linkOffset * 8);
+
+  el = document.getElementById('animContainer');
+  elBound = el.getBoundingClientRect();
+  drawX = elBound.left;
+  drawY = elBound.top;
+  drawW = elBound.right-elBound.left;
+  drawH = elBound.bottom-elBound.top;
+
+  background(248, 251, 252);
+  background(248, 251, 252, 50);
 }
